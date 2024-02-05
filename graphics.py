eca3461e4a47bd2e285a4dbcab242970dede502b
Leonardo Diniz <leonardodiber@gmail.com>
2024-02-04 21:18:05 -0300

    ✨ feat: creates Point and Line classes

--- graphics.py
+++ graphics.py
@@ -21,3 +21,28 @@ class Window:
 
     def close(self):
         self.__is_running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, point_a, point_b):
+        self.p1 = point_a
+        self.p2 = point_b
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.p1.x,
+            self.p1.y,
+            self.p2.x,
+            self.p2.y,
+            fill=fill_color,
+            width=2,
+        )
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 
 def main():
     win = Window(800, 600)
+    win.draw_line(Line(Point(0, 0), Point(300, 100)), "red")
     win.wait_for_close()
 
 
